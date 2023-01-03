fa5140f035eb92ecf8ea619df6ad156ab33a8f92
HariprakashM <hariprakashm20@gmail.com>
2023-01-03 08:58:44 +0530

    vercel links updated

--- src/pages/Myprojects.js
+++ src/pages/Myprojects.js
@@ -5,7 +5,8 @@ const Myprojects = [
       image: "./cv.png",
       description:
         "A Resume builder application where resume templates are widely available and user can select the liked one and can print the resume which is developed using react , node , Express  and mongodb ",
-      link1: "https://pixel-cv.netlify.app/login",
+      link1: "https://resume-builder-frontend-silk.vercel.app/login",
+      // link1: "https://pixel-cv.netlify.app/login",
       link2: "https://github.com/HariprakashM/resume-builder-frontend",
       link3: "https://github.com/HariprakashM/resume-builder-backend",
     },
@@ -14,7 +15,8 @@ const Myprojects = [
       image: "./rooms.png",
       description:
         "A Room booking application where user can book room, add room, delete room, search & filter rooms and view the booking status of each rooms which is developed using react , node , Express  and mongodb ",
-      link1: "https://pixel-rooms.netlify.app/",
+      link1: "https://pixelrooms-frontend.vercel.app/",
+      // link1: "https://pixel-rooms.netlify.app/",
       link2: "https://github.com/HariprakashM/pixelrooms-frontend",
       link3: "https://github.com/HariprakashM/pixelrooms-backend",
     },
@@ -23,7 +25,8 @@ const Myprojects = [
       image: "./stream.png",
       description:
         "An video streaming application where user can add, edit, delete, view video using react-player and register & login using JWT token  developed using react , node , Express and mongodb",
-      link1: "https://pixel-stream.netlify.app/",
+      link1: "https://pixel-stream-frontend.vercel.app/",
+      // link1: "https://pixel-stream.netlify.app/",
       link2: "https://github.com/HariprakashM/pixel-stream-frontend",
       link3: "https://github.com/HariprakashM/pixel-stream-backend",
     },
@@ -32,7 +35,8 @@ const Myprojects = [
         image: "./shops.png",
         description:
           "An Ecommerce application developed using redux where you can add items to cart, pay using stripe-payment method and admin can add, edit, delete and update products and can see all the orders",
-        link1: "https://pixel-shops.netlify.app/",
+        link1: "https://pixel-ecommerce-frontend.vercel.app/",
+        // link1: "https://pixel-shops.netlify.app/",
         link2: "https://github.com/HariprakashM/pixel-ecommerce-frontend",
         link3: "https://github.com/HariprakashM/pixel-ecommerce-backend",
       },
@@ -41,7 +45,8 @@ const Myprojects = [
       image: "./chat.png",
       description:
         "A chat application where we can chat with our buddies and families. We got Rooms where we can talk about general stuffs which is developed using react , node , Express  and mongodb",
-      link1: "https://pixel-chat.netlify.app/",
+      link1: "https://pixel-chat-backend-1.vercel.app/",
+      // link1: "https://pixel-chat.netlify.app/",
       link2: "https://github.com/HariprakashM/pixel-chat-frontend",
       link3: "https://github.com/HariprakashM/pixel-chat-backend-1",
     }
